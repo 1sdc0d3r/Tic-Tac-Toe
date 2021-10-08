b573a4ecd00cdc04d40e0d2d922a5e54ae68190e
Braden Bell <fish0859@gmail.com>
2021-10-08 17:29:56 -0600

    game functionality working, board populating properly

--- components/board.tsx
+++ components/board.tsx
@@ -1,18 +1,36 @@
 export default function Board(props: any) {
   //? typescript type with extracting tileCount
   const tileCount: number = props.tileCount;
+  const tileState: any = props.tileState; //? typescript any->object
+  const player: number = props.player;
+  const toggleTile: (player: number, tileNumber: number) => void =
+    props.toggleTile;
+
   const tiles = Array.from(Array(tileCount).keys(), (i) => i + 1); //creates array starting with 1
   return (
     <div className="board-wrapper w-3/4 h-3/4 border-2 border-solid border-black flex flex-wrap m-auto ">
-      {tiles.map((e, i) => tile(e, i))}
+      {tiles.map((e, i) => tile(e, i, tileState[e], player))}
     </div>
   );
-}
 
-const tile = (tileNumber: number, iterator: number) => {
-  return (
-    <div className="tile test-border w-1/3 h-1/3" key={iterator}>
-      <p className="text-red-600">{tileNumber}</p>
-    </div>
-  );
-};
+  function tile(
+    tileNumber: number,
+    iterator: number,
+    state: string,
+    player: number
+  ) {
+    //   const color = iterator % 2 ? 'black' : 'white';
+    // console.log({ tileNumber, state });
+    return (
+      <div
+        className={`tile test-border w-1/3 h-1/3 flex overflow-hidden`}
+        key={iterator}
+        onClick={() => toggleTile(player, tileNumber)}
+      >
+        <p className="text-red-600 text-9xl w-win h-min m-auto text-center">
+          {state}
+        </p>
+      </div>
+    );
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,63 @@
 import type { NextPage } from 'next';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Board from '../dist/board';
 
 const Home: NextPage = () => {
+  const [tileState, setTileState] = useState({});
+  const [activePlayer, setActivePlayer] = useState(0);
+  const [moveCount, setMoveCount] = useState(0);
+  const [gameActive, setGameActive] = useState(true);
+  const [gameOutcome, setGameOutcome] = useState(null);
+
+  function toggleTile(player: number, tileNumber: number): void {
+    if (!tileState[tileNumber] && gameActive) {
+      // Only run function if tile has not been selected
+      const symbol = player ? 'X' : 'O';
+      setActivePlayer(player ? 0 : 1);
+      setTileState({ ...tileState, [tileNumber]: symbol });
+      setMoveCount(moveCount + 1);
+    } else if (!gameActive) console.log('game already over');
+    else console.log('tile taken');
+  }
+
+  useEffect(() => {
+    //check win
+    const possibleWins: number[][] = [
+      [1, 2, 3], //row-top
+      [4, 5, 6], //row-middle
+      [7, 8, 9], //row-bottom
+      [1, 4, 7], //column-left
+      [2, 5, 8], //column-middle
+      [3, 6, 9], //column-right
+      [1, 5, 9], //diagonal-topL-bottomR
+      [3, 5, 7], //diagonal-topR-bottomL
+    ];
+
+    if (Object.keys(tileState).length > 3) {
+      possibleWins.forEach((arr) => {
+        const values: number[] = arr.map((t: number) => tileState[t]);
+        if (
+          !values.includes(undefined) &&
+          values[0] === values[1] &&
+          values[0] === values[2]
+        ) {
+          console.log('WIN');
+          setGameActive(false);
+          setGameOutcome(values[0]);
+        }
+      });
+      if (moveCount === 9) console.log('staleMate');
+    }
+  }, [tileState, moveCount]);
+
+  function resetGame() {
+    setTileState({});
+    setActivePlayer(0);
+    setMoveCount(0);
+    setGameActive(true);
+  }
+
   return (
     <div className="h-screen">
       <Head>
@@ -11,8 +66,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="h-full test-border">
-        <Board tileCount={9} />
+      <main className="h-full w-full test-border flex flex-col">
+        <h2>Player Turn: {activePlayer ? 'X' : 'O'}</h2>
+        <Board
+          tileCount={9}
+          tileState={tileState}
+          player={activePlayer}
+          toggleTile={toggleTile}
+        />
+        <button onClick={resetGame}>Reset</button>
+        {/* //! change color on win/stalemate */}
         {/* <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
